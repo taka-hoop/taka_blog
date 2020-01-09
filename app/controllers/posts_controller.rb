766121e210ee9cd83d78b511ea01eae1ae3eed6a
taka-hoop <takanari_gal1318@ezweb.ne.jp>
2020-01-09 15:07:41 +0900

    edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,26 @@ class PostsController < ApplicationController
     redirect_to root_path
   end
 
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to root_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
   private
   def post_params
     params.require(:post).permit(:title, :content)
